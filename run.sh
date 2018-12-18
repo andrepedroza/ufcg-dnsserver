b34c6ead60fdb88d3424b81036cd0732ac2a2d47
André Pedroza <contact@andrepedroza.com>
2018-12-18 15:06:41 -0300

    Fix volume bind bug

--- run.sh
+++ run.sh
@@ -6,6 +6,18 @@ else
   export CLUSTER_MODE=1
 fi
 
-echo "cluster_mode = $CLUSTER_MODE" >> /nxfilter/conf/cfg.properties
+if grep -Fxq "cluster_mode" /nxfilter/conf/cfg.properties;then
+  echo "Cluster mode found, skip..."
+else
+  echo "Settting up Cluster mode to $CLUSTER_MODE"
+  echo "cluster_mode = $CLUSTER_MODE" >> /nxfilter/conf/cfg.properties
+fi
+
+if [ -e /nxfilter/conf/uid.txt ]; then
+  echo "Files found, skip..."
+else
+  echo "First boot, coping default files..."
+  cp -R /nxfilter/default/* /nxfilter/
+fi
 
 supervisord
